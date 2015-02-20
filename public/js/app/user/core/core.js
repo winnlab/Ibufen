4d8877d72c2cfef562c228d9fdad9b96fca0051d
HydraOrc <hydra0@bigmir.net>
2015-02-20 21:20:53 +0200

    handle sopelka link

--- public/js/app/user/core/core.js
+++ public/js/app/user/core/core.js
@@ -134,9 +134,7 @@ var Core = can.Control.extend(
 		
 		'a.sopelka click': function(el, ev) {
 			ev.preventDefault();
-			
-			var win = window.open('/sopelka', '_blank');
-			return win.focus();
+			window.location = '/sopelka';
 		}
 	}
 );
